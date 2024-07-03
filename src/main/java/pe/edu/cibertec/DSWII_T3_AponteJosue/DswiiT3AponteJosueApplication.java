9eeaaa7f14b0c3a07ef10b04a59b1fd000b41616
JosueA3 <josueaponter@gmail.com>
2024-07-03 00:49:16 -0500

    Pregunta 3 y 4 100%

--- src/main/java/pe/edu/cibertec/DSWII_T3_AponteJosue/DswiiT3AponteJosueApplication.java
+++ src/main/java/pe/edu/cibertec/DSWII_T3_AponteJosue/DswiiT3AponteJosueApplication.java
@@ -2,6 +2,9 @@ package pe.edu.cibertec.DSWII_T3_AponteJosue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DswiiT3AponteJosueApplication {
@@ -9,5 +12,15 @@ public class DswiiT3AponteJosueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DswiiT3AponteJosueApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer configGlobalCors(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT")
+						.allowedOrigins("https://www.cibertec.edu.pe");
+			}
+		};
+	}
 }
