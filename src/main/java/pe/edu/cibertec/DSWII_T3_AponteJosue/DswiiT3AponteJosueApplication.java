336e30c741fa73ce70e1f616f420360dfab5d737
JosueA3 <josueaponter@gmail.com>
2024-07-02 23:07:00 -0500

    Pregunta1

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_AponteJosue/DswiiT3AponteJosueApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_AponteJosue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3AponteJosueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3AponteJosueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_T3_AponteJosue/DswiiT3AponteJosueApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_AponteJosue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3AponteJosueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
